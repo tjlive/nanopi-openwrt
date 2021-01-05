af08a944f09c39880d7df783d7ea51c4357b673d
潘羿 <33997505+paniy@users.noreply.github.com>
2021-01-05 19:54:27 +0800

    Update 3_prepare_packages.sh

--- 3_prepare_packages.sh
+++ 3_prepare_packages.sh
@@ -15,10 +15,10 @@ rm -rf luci-theme-argon*/ ../lean/luci-theme-argon/ && git clone -b 18.06 https:
 #rm -rf luci-theme-infinityfreedom/ && git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
 rm -rf luci-app-chinadns-ng/ && git clone https://github.com/WuSiYu/luci-app-chinadns-ng
 rm -rf openwrt-chinadns-ng/ && git clone https://github.com/pexcn/openwrt-chinadns-ng
-rm -rf v2ray/ && svn co https://github.com/coolsnowwolf/lede/tree/master/package/lean/v2ray
-rm -rf v2ray-plugin/ && svn co https://github.com/coolsnowwolf/lede/tree/master/package/lean/v2ray-plugin
-rm -rf xray/ && svn co https://github.com/coolsnowwolf/lede/tree/master/package/lean/xray
-rm -rf trojan/ && svn co https://github.com/coolsnowwolf/lede/tree/master/package/lean/trojan
+rm -rf v2ray/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray
+rm -rf v2ray-plugin/ && svn co https://github.com/coolsnowwolf/lede/trunk//package/lean/v2ray-plugin
+rm -rf xray/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/xray
+rm -rf trojan/ && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan
 git clone https://github.com/songchenwen/nanopi-r2s
 
 ##smartdns
